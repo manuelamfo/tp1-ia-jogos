e1379cbc0c9b1adde6acc5f68400ef5828067aa3
manuelamfo <manuelamfo@gmail.com>
2026-05-10 17:24:30 -0300

    minimax e evaluate melhorando, pode melhorar (código eu e daniel)

--- search.py
+++ search.py
@@ -1,3 +1,6 @@
+from curses import window
+import enum
+from hmac import new
 from typing import List, Tuple, Optional, Dict
 import time
 import math
@@ -73,6 +76,80 @@ def other(player: int) -> int:
 # -----------------------------------------------------------------------------
 # ÚNICO PONTO A SER IMPLEMENTADO PELOS ALUNOS
 # -----------------------------------------------------------------------------
+class Agent(enum.Enum): #? possível sugestão: retirar a classe Agent e usar P1 e P2 no lugar (variáveis globais já existentes)
+    MAX = 1 # Jogador que tenta maximizar a heurística
+    MIN = 2 # Jogador que tenta minimizar a heurística
+
+def score_window(window: List[int], turn:int) -> int:
+    player = turn
+    opponent = other(turn)
+
+    player_count = window.count(player)
+    opponent_count = window.count(opponent)
+    empty_count = window.count(EMPTY)
+
+    score_window = 0
+    if player_count == 4: 
+        score_window += 1000
+    if player_count == 3 and empty_count == 1: 
+        score_window += 50
+    if player_count == 2 and empty_count == 2: 
+        score_window += 10
+
+    if opponent_count == 3 and empty_count == 1: 
+        score_window -= 80 # penalidade para oportunidades do oponente
+    if opponent_count == 2 and empty_count == 2: 
+        score_window -= 5
+
+    return score_window
+
+def evaluate(board: List[List[int]], turn: int):
+    total_score = 0
+
+    eval_board = [[3, 4, 5, 7, 5, 4, 3],
+                 [4, 6, 8, 10, 8, 6, 4],
+                 [5, 7, 11, 13, 11, 7, 5],
+                 [5, 7, 11, 13, 11, 7, 5],
+                 [4, 6, 8, 10, 8, 6, 4],
+                 [3, 4, 5, 7, 5, 4, 3]]
+
+    # faz uma avaliação inicial baseada em todas as peças já presente no tabuleiro
+    for r in range(ROWS):
+        for c in range(COLS):
+            # se há uma peça na posição [r][c] e o turno é de max/min, aplica  a avaliação para aquela posição
+            if board[r][c] ==  turn:
+                total_score += eval_board[r][c]
+            elif board[r][c] == other(turn):
+                total_score -= eval_board[r][c]
+    
+    # dá uma pontuação extra caso haja duplas/triplas/quádruplas no tabuleiro
+    # duplas/triplas/quádruplas horizontais
+    for r in range(ROWS):
+        for c in range(COLS - 3):
+            window = [board[r][c+i] for i in range (4)]
+            total_score += score_window(window, turn)
+
+    # duplas/triplas/quádruplas verticais
+    for c in range(COLS):
+        for r in range(ROWS - 3):
+            window = [board[r+i][c] for i in range(4)]
+            total_score += score_window(window, turn)
+
+    # duplas/triplas/quádruplas diagonais ↘
+    for r in range(ROWS - 3):
+        for c in range(COLS - 3):
+            window = [board[r+i][c+i] for i in range(4)]
+            total_score += score_window(window, turn)
+            
+    # duplas/triplas/quádruplas diagonais ↗
+    for r in range(3, ROWS):
+        for c in range(COLS - 3):
+            window = [board[r-i][c+i] for i in range(4)]
+            total_score += score_window(window, turn)
+
+    print(f"Total score para a partida {total_score} - jogador {turn}")
+    return total_score
+
 def choose_move(board: List[List[int]], turn: int, config: Dict) -> Tuple[int, Dict]:
     """
     Decide a coluna (0..6) para jogar agora.
@@ -104,10 +181,49 @@ def choose_move(board: List[List[int]], turn: int, config: Dict) -> Tuple[int, D
         # Sem jogadas: devolve 0 por convenção (servidor lida com isso)
         return move
     
-    # VERSÃO INICIAL: escolhe aleatoriamente entre as jogadas legais
-    move = random.choice(legal)
+    # minimax apenas calcula a melhor jogada possível.
+    # é a função choose_move que deve guardar a melhor jogada encontrada e retornar no final, depois de avaliar todas as jogadas possíveis (dentro do limite de tempo)
+    def minimax(board: List[List[int]], agent: Agent, depth: int, legal_moves: List[int], config: Dict): # passando o dicionário config pra conseguir pegar 
+        
+        #if winner(board) != 0: #! está desconsiderando o empate
+        is_terminal = terminal(board)[0]
+        if is_terminal or time_exceeded() or depth == 0:
+            winner_player = terminal(board)[1]
+            if winner_player == turn:
+                return 1500
+            elif winner_player == other(turn):
+                return -1500
+            elif winner_player == 0:
+                return 0
+            
+            return evaluate(board, turn)
+        
+        if (agent == Agent.MAX):
+            best_score_turn = -math.inf
+            for move in legal_moves:
+                new_board = make_move(board, move, turn) # usando turn ao invés de agent porque o make_move precisa do número do jogador (1 ou 2) e não do MAX/MIN
+                score = minimax(new_board, Agent.MIN, depth - 1, valid_moves(new_board), config)
+                best_score_turn = max(score, best_score_turn)
+        else:
+            best_score_turn = math.inf
+            for move in legal_moves:
+                new_board = make_move(board, move, other(turn)) # usando other(turn) para passar o número do jogador adversário
+                score = minimax(new_board, Agent.MAX, depth - 1, valid_moves(new_board), config)       
+                best_score_turn = min(score, best_score_turn)
+        
+        return best_score_turn
+    
+    # loop para escolher a melhor jogada possível, usando a função minimax para avaliar cada jogada
+    best_move = None
+    best_score = -math.inf
+    for move in legal:
+        new_board = make_move(board, move, turn)
+        score = minimax(new_board, Agent.MIN, max_depth - 1, valid_moves(new_board), config) 
+        if score > best_score:
+            best_score = score
+            best_move = move
 
-    return move
+    return best_move
 
 def choose_move_randomly(board: List[List[int]], turn: int, config: Dict) -> Tuple[int, Dict]:
     max_time_ms = int(config.get("max_time_ms"))
